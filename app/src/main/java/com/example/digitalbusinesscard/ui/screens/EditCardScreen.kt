3d402bc1f03bfcda63125a3e91b442432a41d24d
aminakhtar5995 <aminakhtar5595@gmail.com>
2025-02-20 08:49:33 +0500

    Implemented first name field with icon on add new card screen

--- app/src/main/java/com/example/digitalbusinesscard/ui/screens/EditCardScreen.kt
+++ app/src/main/java/com/example/digitalbusinesscard/ui/screens/EditCardScreen.kt
@@ -1,4 +1,6 @@
 package com.example.digitalbusinesscard.ui.screens
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,7 +10,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material3.Button
@@ -17,6 +21,8 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -24,7 +30,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -67,6 +75,28 @@ fun EditCardScreen (navController: NavController, viewModel: BusinessCardViewMod
             modifier = Modifier
                 .padding(horizontal = 20.dp)
         ) {
+            Row (
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.AccountBox, contentDescription = "First Name",
+                    modifier = Modifier.padding(end = 10.dp))
+
+                TextField(
+                    value = viewModel.fullName,
+                    onValueChange = { viewModel.fullName = it },
+                    colors = TextFieldDefaults.textFieldColors(
+                        containerColor = Color.Transparent,
+                        textColor = Color.Gray,
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        placeholderColor = Color.LightGray
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .border(width = 0.5.dp, color = Color.LightGray),
+                    placeholder = { Text("First Name") },
+                )
+            }
 
 // Commenting old UI
 //        OutlinedTextField(
