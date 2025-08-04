b217c8c3d4437130cdad947ee9c3dba29f22ccb4
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-08-04 10:04:35 +0500

    Start working on menu screen

--- app/src/main/java/com/example/digitalbusinesscard/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/example/digitalbusinesscard/ui/navigation/NavGraph.kt
@@ -1,25 +1,22 @@
 package com.example.digitalbusinesscard.ui.navigation
 import androidx.compose.runtime.Composable
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.digitalbusinesscard.ui.screens.menu.MenuScreen
 import com.example.digitalbusinesscard.ui.screens.splash.SplashScreen
-import com.example.digitalbusinesscard.ui.viewmodels.BusinessCardViewModel
 
 @Composable
 fun AppNavGraph(navController: NavHostController) {
 
-    val businessCardViewModel : BusinessCardViewModel = viewModel()
-
     NavHost(navController = navController, startDestination = Screen.Splash.route) {
         composable(Screen.Splash.route) { SplashScreen() }
+        composable(Screen.Menu.route) { MenuScreen() }
     }
 }
 
 // Enum-like class for routes
 sealed class Screen(val route: String) {
-    object Home : Screen("homeScreen")
-    object Edit : Screen("editCardScreen")
+    object Menu : Screen("menu")
     object Splash : Screen("splash")
 }
--- app/src/main/java/com/example/digitalbusinesscard/ui/screens/menu/MenuScreen.kt
+++ app/src/main/java/com/example/digitalbusinesscard/ui/screens/menu/MenuScreen.kt
@@ -1,2 +1,8 @@
 package com.example.digitalbusinesscard.ui.screens.menu
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MenuScreen() {
+
+}
